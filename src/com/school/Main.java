89aeda90d2c3574d98745feeb3bd35be69ff9f3e
colinvlienden <colinvlienden@users.noreply.github.com>
2021-04-22 16:07:24 +0200

    Add main
    basic print data, change student

---
+++ src/com/school/Main.java
@@ -0,0 +1,11 @@
+package com.school;
+
+public class Main {
+    public static void main(String [] args) {
+        Student student1 = new Student("Colin", "AI1");
+
+        System.out.println(student1.getNaam());
+        System.out.println(student1.getKlas());
+
+    }
+}
--- src/com/school/Student.java
+++ src/com/school/Student.java
@@ -2,13 +2,26 @@ package com.school;
 
 public class Student {
     private String naam;
+    private String klas;
 
-    public Student(String naam) {
-
+    public Student(String naam, String klas) {
         this.naam = naam;
+        this.klas = klas;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public void setKlas(String klas) {
+        this.klas = klas;
     }
 
     public String getNaam() {
         return naam;
+    }
+
+    public String getKlas() {
+        return klas;
     }
 }
